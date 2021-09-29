3b9d38db191cde9755b57d908f09b878a34653f0
takehirodesuyo <takehiro.kt106@gmail.com>
2021-09-29 22:16:29 +0900

    9/29 ２回目　セルパ関数を用いた

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Test;
+
+use Illuminate\Support\Facades\DB;
+
+class TestController extends Controller
+{
+    //
+    public function index()
+    {
+        $values = Test::all();
+
+
+
+        dd($values);
+
+        return view('tests/test', compact('values'));
+    }
+}
--- database/migrations/2021_09_27_231814_create_tests_table.php
+++ database/migrations/2021_09_27_231814_create_tests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTestTab extends Migration
+class CreateTestsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateTestTab extends Migration
      */
     public function up()
     {
-        Schema::create('test_tab', function (Blueprint $table) {
+        Schema::create('tests', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string("text",100);
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreateTestTab extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('test_tab');
+        Schema::dropIfExists('tests');
     }
 }
